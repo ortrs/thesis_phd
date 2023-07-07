646a8ca6e152077cebf8633579d270ba93824abf
Orlando Torres (Adm.) <otorres@ltipc207.lti.local>
2023-07-07 14:32:39 +0200

    added xelatex+biber for one-touch compilation (https://tex.stackexchange.com/questions/127384), added sdq dissertation ohne microtype (xelatex conflict), removed amsmath (conflict sdqdiss), added cm-super for vectorial fonts (https://tex.stackexchange.com/questions/228051)

---
+++ XeLatex+biber.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\MiKTeX\miktex\bin\x64\miktex-xelatex.exe" %1 %2 %3
+"C:\Program Files\MiKTeX\miktex\bin\x64\biber.exe" %3
+"C:\Program Files\MiKTeX\miktex\bin\x64\makeindex.exe" %3
+"C:\Program Files\MiKTeX\miktex\bin\x64\miktex-xelatex.exe" %1 %2 %3 
+"C:\Program Files\MiKTeX\miktex\bin\x64\miktex-xelatex.exe" %1 %2 %3
